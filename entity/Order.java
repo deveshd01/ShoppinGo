d3ba6e302426730cc55d4b6d03d75e6a687ab01a
unknown <iamdeveshd11@gmail.com>
2023-03-09 21:25:58 +0530

    entity added

---
+++ entity/Order.java
@@ -0,0 +1,101 @@
+package com.pratiti.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the order database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Order.findAll", query="SELECT o FROM Order o")
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	@Temporal(TemporalType.DATE)
+	private Date orderDate;
+
+	private int totalPrice;
+
+	private int userId;
+
+	//bi-directional many-to-one association to Address
+	@ManyToOne
+	@JoinColumn(name="addressId")
+	private Address address;
+
+	//bi-directional many-to-one association to Payment
+	@ManyToOne
+	@JoinColumn(name="paymentId")
+	private Payment payment;
+
+	//bi-directional many-to-one association to Product
+	@ManyToOne
+	@JoinColumn(name="productId")
+	private Product product;
+
+	public Order() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getOrderDate() {
+		return this.orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public int getTotalPrice() {
+		return this.totalPrice;
+	}
+
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public int getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public Address getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Payment getPayment() {
+		return this.payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Product getProduct() {
+		return this.product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+}
