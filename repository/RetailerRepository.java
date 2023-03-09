8831c451307f84a81f07c77859c32b8690caf813
unknown <iamdeveshd11@gmail.com>
2023-03-09 21:54:11 +0530

    repository Commit

---
+++ repository/RetailerRepository.java
@@ -0,0 +1,9 @@
+package com.pratiti.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pratiti.entity.Retailer;
+
+public interface RetailerRepository extends JpaRepository<Retailer, Integer>{
+
+}
